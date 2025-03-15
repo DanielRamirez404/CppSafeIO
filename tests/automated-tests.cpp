108428de763cd7a69827f2ef44b3b1234f182c8f
Daniel Ramírez <136678726+DanielRamirez404@users.noreply.github.com>
2025-03-15 16:37:13 -0400

    Improve wording for the messages that get displayed when the tests pass or fail

--- tests/automated-tests.cpp
+++ tests/automated-tests.cpp
@@ -10,12 +10,12 @@ int main()
             CppSafeIO::clearConsole(); //not executing so that the building and testing log doesn't get erased in case of running ctest -V
         
         std::cout << "Automated tests passed.\n"
-                  << "You can include the cppsafeio.h file, and use the CppSafeIO namespace.\n"
+                  << "You can include the cppsafeio.h header file and use the CppSafeIO namespace.\n"
                   << "You can try to run CppSafeIO-Tests executable to test the input functions.\n";
     }
     catch (const std::exception& exception)
     {
-        std::cerr << "CppSafeIO library's tests failed\n"
+        std::cerr << "CppSafeIO library's automated tests failed\n"
                   << "Error: " << exception.what() << '\n';
     
         return 1;
